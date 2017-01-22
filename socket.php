fa789ad07ce378b2cbdaeed26876a2960eeec413
zhaohehe <zhaohehedola@gmail.com>
2017-01-22 14:15:26 +0800

    home update configuable

--- socket.php
+++ socket.php
@@ -7,7 +7,5 @@ require 'vendor/autoload.php';
 
 use Pad\Foundation\Socket\Server;
 
-$config_file = require __DIR__.'/../../config.php';    //require config file
-
 $socket = new server();
 $socket->start();
